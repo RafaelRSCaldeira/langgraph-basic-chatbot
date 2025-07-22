142c6c3a8a8eab4daf2fcd8e4700a433dc2bd274
RafaelRSCaldeira <rafael.caldeira.2005@gmail.com>
2025-07-22 11:27:22 -0300

    feat: add system message on the llm call

--- nodes/chatbot.py
+++ nodes/chatbot.py
@@ -1,7 +1,14 @@
 from models.openai_model import OpenAIModel
 from state.state import State
+from langchain_core.messages import SystemMessage
 
 
 def chatbot(state: State):
+    # Initialize model
     model = OpenAIModel()
-    return { "messages": [model.llm_with_tools.invoke(state["messages"])] }
+
+    # System message
+    sys_msg = SystemMessage(
+    content="You are a helpful assistant that provides accurate responses based on the given tools.")
+
+    return { "messages": [model.llm_with_tools.invoke([sys_msg] + state["messages"])] }
