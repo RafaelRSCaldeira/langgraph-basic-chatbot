3bcbd91efc0b5524bf8eb68305a686d99197978f
RafaelRSCaldeira <rafael.caldeira.2005@gmail.com>
2025-07-22 11:28:06 -0300

    fix: graph now can call more than one tool with a single user input

--- graph/graph.py
+++ graph/graph.py
@@ -1,5 +1,7 @@
 from langgraph.graph import StateGraph, START, END
 from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.checkpoint.memory import MemorySaver 
+
 from nodes.chatbot import chatbot
 from state.state import State
 from tools.tools import tools_list
@@ -7,11 +9,9 @@ from tools.tools import tools_list
 # Initialize graph builder
 graph_builder = StateGraph(State)
 
-
 # Add nodes
 graph_builder.add_node("chatbot", chatbot)
 graph_builder.add_node("tools", ToolNode(tools_list))
-
 
 # Add edges
 graph_builder.add_edge(START, "chatbot")
@@ -19,7 +19,10 @@ graph_builder.add_conditional_edges(
     "chatbot",
     tools_condition
 )
-graph_builder.add_edge("tools", END)
+graph_builder.add_edge("tools", "chatbot")
 
-# Compile graph
+# Initialize memory
+memory = MemorySaver()
+
+# Compile graph with memory
 graph = graph_builder.compile()
